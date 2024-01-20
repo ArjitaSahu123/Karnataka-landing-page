6e0376c1fdeb73cd7220e87910deac946737ad60
unknown <2k22.cse.2213081@gmail.com>
2024-01-20 18:54:19 +0530

    landing-page

---
+++ index.js
@@ -0,0 +1,13 @@
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+  })
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
